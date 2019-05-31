d4dbb1934132e088bdeb144456e3b203a54fe8cf
MKPapelaria <MKPapelaria@192.168.250.3>
2019-05-31 18:03:01 -0300

    Nova tela de entrada finalizada

--- src/com/sert/main/Main.java
+++ src/com/sert/main/Main.java
@@ -6,13 +6,24 @@ import com.sert.telas.Entrada;
  * Desenvolvido e mantido por SertSoft -- Uma empresa do gupo M&K
  * 
  * @author Matheus Souza
- * @version 1.0.5
- * OC HONDA: 2432057
+ * @version 1.1.0 OC HONDA: 2432057. 
+ * Funcionamento da numeração de versionamento do sistema: 
+ * 			1- O primeiro numero significa que uma mudança de grande
+ *          significancia foi adicionada ao sistema como um novo
+ *          modulo, novas funcionalidades de peso.
+ *          2- O segundo numero significa mudanças nas telas onde
+ *          o usuário sentirá uma real diferença como mudança total 
+ *          ou pelo menos mudança de 50% da tela.
+ *          3- O terceiro numero indicará mudanças de pequeno porte, 
+ *          para controle apenas interno, significando mudanças de rotinas
+ *          melhoras de desenpenho, otimização de codigo, pequenas mudanças na tela 
+ *          R- O R significa o release e não será divulgado ao Usuário, apenas para uso interno
+ *          e controle de qualquer outra modificação que não se enquadre em alguma das 3 opções
+ *          anteriores.
  * 
  */
 public class Main {
 	public static void main(String[] args) {
-		
 		try {
 			new Entrada().setVisible(true);
 		} catch (Exception e) {
---
+++ src/com/sert/main/Teste.java
@@ -0,0 +1,109 @@
+package com.sert.main;
+import java.awt.print.PrinterJob;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import javax.print.PrintService;
+import javax.swing.JOptionPane;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+//		for (PrintService p : PrinterJob.lookupPrintServices()) {  
+//            System.out.println(p.getName());  
+//		}
+		
+		// ESCREVER TXT
+		try {
+			File arquivo = new File("teste.txt");
+			if (arquivo.exists()) {
+				// se existir
+				FileWriter arquivoTxt = new FileWriter(arquivo, true);
+				PrintWriter linhasTxt = new PrintWriter(arquivoTxt);
+				// ACREDITO QUE SO PODE TER 42 CARACTERES
+				linhasTxt.println("==========================================");
+				linhasTxt.println("              Nome da empresa              ");
+				linhasTxt.println("===========================================");
+				linhasTxt.println("********** NAO E DOCUMENTO FISCAL *********");
+				linhasTxt.println("===========================================");
+				linhasTxt.println("PRODUTO      QTDE      VALOR UN.      VALOR");
+				// dados da tabela
+				/*
+				 * for(int x = 0; x < tabelaConsumo.getRowCount(); x++){
+				 * linhasTxt.print(String.format("%-10.10s",tabelaConsumo.getModel().getValueAt(
+				 * x, 1)));
+				 * linhasTxt.print(String.format("%7s     ",tabelaConsumo.getModel().getValueAt(
+				 * x, 5)));
+				 * linhasTxt.print(String.format("%10s    ",tabelaConsumo.getModel().getValueAt(
+				 * x, 4)));
+				 * linhasTxt.print(String.format("%7s    ",tabelaConsumo.getModel().getValueAt(
+				 * x, 6))); linhasTxt.println(); }
+				 */
+				linhasTxt.println("===========================================");
+				linhasTxt.println("   INFORMACOES PARA FECHAMENTO DE CONTA    ");
+				linhasTxt.println("===========================================");
+				linhasTxt.println(" GARCOM  CONTA.DIV.  VAL.PESS. COVER  DESC.");
+				linhasTxt.print(String.format("%7s  %9s  %9s  %5s   %s", "garcom", "dividirconta", "vlpessoa", "cover",
+						"desconto"
+				// garcom.getText(),
+				// dividirConta.getText(),
+				// valorPorPessoa.getText(),
+				// cover.getText(),
+				// descontoConta.getText()
+				));
+				linhasTxt.println();
+				linhasTxt.println("===========================================");
+				// linhasTxt.println("SubTotal " + valorBruto.getText());
+				linhasTxt.println("SubTotal                            " + "10,00");
+				linhasTxt.println("                   ------------------------");
+				// linhasTxt.println("Total " + valorBruto.getText());
+				linhasTxt.println("Total                                " + "10,00");
+				linhasTxt.println("===========================================");
+				linhasTxt.println("       MENSAGEM DA EMPRESA VAI AQUI        ");
+				linhasTxt.println("   INFORMACOES PARA FECHAMENTO DE CONTA    ");
+
+				int i = 0;
+				while (i < 10) {
+					i++;
+					linhasTxt.println();
+				}
+				arquivoTxt.close();
+				// emiteComanda();
+
+			} else {
+				// se naum existir
+				arquivo.createNewFile();
+				// criaTxt();
+			}
+		} catch (IOException error) {
+			System.out.println("nao encontrei arquivo");
+		}
+
+		// imprime arquivo
+		try {
+			InputStream is = new FileInputStream("teste.txt");
+			Scanner sc = new Scanner(is);
+			System.out.println("Imprimindo...");
+			FileOutputStream fs = new FileOutputStream("ESDPRT001");
+			PrintStream ps = new PrintStream(fs);
+
+			while (sc.hasNextLine()) {
+				String linhas = sc.nextLine();
+				ps.println(linhas);
+			}
+			fs.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Erro encontrado ao imprimir comanda.");
+		}
+
+	}
+}
